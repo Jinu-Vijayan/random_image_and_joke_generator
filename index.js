7a75b938a1636ee89c89741dc52d4ff6560d4059
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-05-10 20:09:50 +0530

    Add: added api to get random dad joke

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const PORT = 4000;
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
+const API_NINJA_XAPI_KEY = process.env.API_NINJA_X_API_KEY;
 
 const app = express();
 
@@ -28,6 +29,26 @@ app.get("/random-image",async (req,res)=>{
         console.log(error);
         res.status(500).json({
             message : 'internal server error occured'
+        })
+    }
+});
+
+app.get('/dad-joke', async (req,res) => {
+    try{
+        const url = 'https://api.api-ninjas.com/v1/dadjokes';
+        const resp = await axios({
+            method : "get",
+            url : url,
+            headers : {
+                'X-Api-Key' : API_NINJA_XAPI_KEY
+            }
+        })
+        res.status(200).json(resp.data[0]);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            error: "Internal server error occured"
         })
     }
 })
